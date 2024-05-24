93ed23ad1a3ddcd510807d110c7801408ae9ccfc
Vivixing <echeverri.vivian1120@gmail.com>
2024-05-24 14:48:44 -0500

    Creado el Main para probrar el uso de la aplicación

--- PatrónComportamiento/practicastate/src/main/java/com/echeverri/Main.java
+++ PatrónComportamiento/practicastate/src/main/java/com/echeverri/Main.java
@@ -2,6 +2,18 @@ package com.echeverri;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user = new User();
+
+        System.out.println("Estado inicial: ");
+        System.out.println("Activar: " + user.Activate());
+        System.out.println("Comprar: " + user.Buy());
+        System.out.println("Bloquear: " + user.lock());
+        System.out.println("Desactivar: " + user.Disable());
+
+        System.out.println("\nDespués de algunas acciones: ");
+        System.out.println("Activar: " + user.Activate());
+        System.out.println("Comprar: " + user.Buy());
+        System.out.println("Bloquear: " + user.lock());
+        System.out.println("Desactivar: " + user.Disable());
     }
 }
